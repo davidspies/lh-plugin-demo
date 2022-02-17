a4b538210cfe53c96fc8eea6efbbab657a27b3a6
David Spies <dnspies@gmail.com>
2022-02-16 20:30:22 -0800

    Instance for M1 causes crash

--- src/Demo/Lib.hs
+++ src/Demo/Lib.hs
@@ -1,7 +1,15 @@
 module Demo.Lib where
 
+import GHC.Generics
+
 {-@ type Pos = {v:Int | 0 < v} @-}
 
 {-@ incr :: Pos -> Pos @-}
 incr :: Int -> Int
 incr x = x + 1
+
+class Foo a where
+  bar :: (a p -> b) -> a p -> b
+
+instance Foo f => Foo (M1 i c f) where
+  bar f = bar (f . M1) . unM1
